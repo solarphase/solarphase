3391a7553af3ec59c9bcfc06486006335a3cf08e
Kimmy Andersson <cyaspik@gmail.com>
2015-03-26 21:45:33 +0100

    Generated a migration for the supplied default user model and tweaked the model to fit the needs of the application.

--- app/User.php
+++ app/User.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+class User extends Model implements AuthenticatableContract {
 
-	use Authenticatable, CanResetPassword;
+	use Authenticatable, SoftDeletes;
 
 	/**
 	 * The database table used by the model.
@@ -22,7 +21,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 *
 	 * @var array
 	 */
-	protected $fillable = ['name', 'email', 'password'];
+	protected $fillable = ['display_name', 'email', 'password'];
 
 	/**
 	 * The attributes excluded from the model's JSON form.
---
+++ database/migrations/2015_03_26_203536_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table)
+		{
+			$table->increments('id');
+			$table->string('display_name', 32);
+			$table->string('email', 255);
+			$table->string('password', 64);
+			$table->rememberToken();
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
