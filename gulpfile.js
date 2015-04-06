bb19bffd2310f48c46935621d2b4e533c27437fa
Kimmy Andersson <cyaspik@gmail.com>
2015-04-06 21:21:01 +0200

    Added the pre-made assets for the website interface.

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,31 @@ var elixir = require('laravel-elixir');
  |
  */
 
+var paths = {
+	'bootstrap': './vendor/bower_components/bootstrap/',
+	'jquery': './vendor/bower_components/jquery/',
+	'sourceSansPro': './vendor/bower_components/fontface-source-sans-pro/',
+	'commonmark': './vendor/bower_components/commonmark/'
+};
+
+// Styles.
 elixir(function(mix) {
-    mix.less('app.less');
+    mix.less('app.less', 'public/css/', {
+		paths: [
+			paths.bootstrap + 'less',
+			paths.sourceSansPro + 'less'
+		]
+	});
 });
+
+// Vendor.
+elixir(function(mix) {
+	mix.copy(paths.bootstrap + 'dist/fonts/**', 'public/fonts')
+		.copy(paths.sourceSansPro + 'fonts/**', 'public/fonts')
+		.scripts([
+			paths.jquery + 'dist/jquery.js',
+			paths.bootstrap + 'dist/js/bootstrap.js',
+			paths.commonmark + 'index.js'
+		], 'public/js/vendor.js', './');
+});
+
