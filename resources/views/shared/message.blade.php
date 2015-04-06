3941eba6b5414f0dd3cdf6de96efb9b9c48e199b
Kimmy Andersson <cyaspik@gmail.com>
2015-04-07 01:48:01 +0200

    Fixed top margin for flash messages.

--- resources/views/shared/message.blade.php
+++ resources/views/shared/message.blade.php
@@ -1,5 +1,5 @@
 @if (Session::has('message.text') && Session::has('message.type'))
-	<p class="alert alert-{{ Session::get('message.type') }}">
+	<p class="flash alert alert-{{ Session::get('message.type') }}">
 		{{ Session::get('message.text') }}
 	</p>
 @endif
