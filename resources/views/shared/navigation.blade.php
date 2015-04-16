be425f50e16cd879943e232e725b041fd4abe0c7
Kimmy Andersson <cyaspik@gmail.com>
2015-04-16 20:59:54 +0200

    Navigation now aligns to the right to make it fit better with the content.

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -1,11 +1,10 @@
-<div class="logo text-center">
-	<a href="/">
-		<img src="{{ URL::asset('img/logo.png') }}" alt="SolarPhase">
-	</a>
-</div>
-{{-- <hr/> --}}
-
 <nav>
+	<div class="logo">
+		<a href="/">
+			<img src="{{ URL::asset('img/logo.png') }}" alt="SolarPhase">
+		</a>
+	</div>
+
 	<ul>
 		@foreach ($navigation_links as $link)
 			@include('shared.link', ['link' => $link])
